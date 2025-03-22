6a8fc6535ceb9ad83e3824c957e3fce2aaa0caac
mDaHua <148463115+mDaHua@users.noreply.github.com>
2025-03-22 20:42:05 +0800

    Delete model_2 directory

--- model_2/conv.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-def Conv3(in_channels, out_channels):
-    return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, 3, 1, 1),
-        nn.LeakyReLU()
-    )
